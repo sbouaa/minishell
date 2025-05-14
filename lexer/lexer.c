d707de81c0ef0aec42e2c8efcb39676cfda8b1ce
Msaq Amine <122275971+amineleet@users.noreply.github.com>
2025-05-14 22:57:11 +0100

    minishell

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -2,19 +2,24 @@
 
 int	lexer(t_data *data)
 {
-	int		i;
-	char	*line;
+	int		i = 0;
+	char	*line = data->prompt;
 
-	i = 0;
-	line = data->prompt;
 	data->token_list = NULL;
 	while (line[i])
 	{
-		if (is_token(line[i]))
+		if (is_space(line[i]))
+			i++;
+		else if (is_token(line[i]))
 			handle_tokens(data, line, &i);
-		else if (handle_word_segments(data, line, &i))
-			return (1);
-		i++;
+		else if (is_quote(line[i]))
+		{
+			if (handle_quote_part(data, line, &i))
+				return (1);
+		}
+		else
+			handle_word_segments(data, line, &i);
 	}
 	return (0);
 }
+
